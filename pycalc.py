e477936b76afc7dff25835ad8fdf4f79743fc330
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-03 06:54:41 +0100

    Refactor calculator logic into a main function for improved structure

--- pycalc.py
+++ pycalc.py
@@ -3,33 +3,37 @@ import os
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-while True:
-    clear_screen()
-
-    try:
-        num1 = float(input("Enter first number: "))
-        op = input("Enter operator (+, -, *, /): ")
-        num2 = float(input("Enter second number: "))
-
-        if op == "+":
-            print(num1 + num2)
-        elif op == "*":
-            print(num1 * num2)
-        elif op == '-':
-            print(num1 - num2)
-        elif op == "/":
-            if num2 == 0:
-                print("Cannot divide by zero")
-            else:
-                print(num1 / num2)
-        else:
-            print("Invalid operator")
-
-    except ValueError:
-        print("Invalid input. Please enter numbers only.")
-    except Exception as e:
-        print(f"An unexpected error occured: {e}")
-
-    if input("Would you like to Continue? (y/n): ").lower() != 'y':
+def calc_main():
+    while True:
         clear_screen()
-        break
+
+        try:
+            num1 = float(input("Enter first number: "))
+            op = input("Enter operator (+, -, *, /): ")
+            num2 = float(input("Enter second number: "))
+
+            if op == "+":
+                print(num1 + num2)
+            elif op == "*":
+                print(num1 * num2)
+            elif op == '-':
+                print(num1 - num2)
+            elif op == "/":
+                if num2 == 0:
+                    print("Cannot divide by zero")
+                else:
+                    print(num1 / num2)
+            else:
+                print("Invalid operator")
+
+        except ValueError:
+            print("Invalid input. Please enter numbers only.")
+        except Exception as e:
+            print(f"An unexpected error occured: {e}")
+
+        if input("Would you like to Continue? (y/n): ").lower() != 'y':
+            clear_screen()
+            break
+
+if __name__ == "__main__":
+    calc_main()
